0b87417aff488fd10f0285441233052f660a1146
bhimsur <bhimsur>
2023-07-26 21:43:07 +0700

    add validation

--- src/main/java/io/bhimsur/rnd/qr/object/ILV.java
+++ src/main/java/io/bhimsur/rnd/qr/object/ILV.java
@@ -1,19 +1,37 @@
 package io.bhimsur.rnd.qr.object;
 
+import io.bhimsur.rnd.qr.constant.QrConstant;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Map;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
+@Valid
 public class ILV implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @NonNull
+    @Size(min = 2, max = 2)
+    @Pattern(regexp = QrConstant.Regex.TWO_DECIMAL, message = QrConstant.Message.WRONG_INPUT)
     private String id;
+
+    @NonNull
+    @Size(min = 2, max = 2)
+    @Pattern(regexp = QrConstant.Regex.TWO_DECIMAL, message = QrConstant.Message.WRONG_INPUT)
     private String length;
+
+    @NonNull
+    @NotEmpty
     private String value;
     private Map<String, ILV> subTag;
 
