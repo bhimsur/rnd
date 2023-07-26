1c2d8509b4ea5153736ccb4d542be25a671ac124
bhimsur <bhimsur>
2023-07-26 21:39:40 +0700

    add exception

---
+++ src/main/java/io/bhimsur/rnd/qr/exception/QrCountryException.java
@@ -0,0 +1,7 @@
+package io.bhimsur.rnd.qr.exception;
+
+public class QrCountryException extends RuntimeException {
+    public QrCountryException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/io/bhimsur/rnd/qr/exception/QrProviderException.java
@@ -0,0 +1,7 @@
+package io.bhimsur.rnd.qr.exception;
+
+public class QrProviderException extends RuntimeException {
+    public QrProviderException(String message) {
+        super(message);
+    }
+}
